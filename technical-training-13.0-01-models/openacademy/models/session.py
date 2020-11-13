200150928042764e6c878ecf6c40dec76e9e5a89
florvhee <florvhee@gmail.com>
2020-11-13 11:21:34 +0100

    courses

--- technical-training-13.0-01-models/openacademy/models/session.py
+++ technical-training-13.0-01-models/openacademy/models/session.py
@@ -7,6 +7,7 @@ class Session(models.Model):
 
     course_id = fields.Many2one(
         string="Course", comodel_name="openacademy.course", required=True)
+
     maester_id = fields.Many2one(
         string="Maester", comodel_name="res.partner", required=True)
 
