b96da70b2cf0153f8ab8d2907a6e6e9344a50da0
florvhee <florvhee@gmail.com>
2020-11-13 11:48:32 +0100

    inherit maester

---
+++ technical-training-13.0-01-models/openacademy/models/maester.py
@@ -0,0 +1,10 @@
+from odoo import models, fields
+
+
+class Maester(models.Model):
+    _name = "openacademy.maester"
+    _inherit = "res.partner"
+    _description = "Maester assigned to a session"
+
+    session_ids = fields.One2many(
+        comodel_name="openacademy.session", inverse_name="course_id", string="Sessions")
