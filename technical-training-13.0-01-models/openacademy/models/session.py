4d77329610fc3afde16e864bc9d74d3b6dc0b56b
Thibault Hermans <hermansthibault@gmail.com>
2020-11-13 16:18:58 +0100

    context

--- technical-training-13.0-01-models/openacademy/models/session.py
+++ technical-training-13.0-01-models/openacademy/models/session.py
@@ -1,11 +1,13 @@
 from odoo import models, fields
+import logging
 
+_logger = logging.getLogger(__name__)
 
 class Session(models.Model):
     _name = "openacademy.session"
     _description = "Session of a course"
     _defaults = {
-        "course_id": lambda self,cr,uid,c:print(c)
+        "course_id": lambda self,cr,uid,c:_logger.info(c)
     }
 
     course_id = fields.Many2one(
