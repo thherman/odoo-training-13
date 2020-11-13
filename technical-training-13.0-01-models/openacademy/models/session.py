527729d220ec1ec046259d3012270c04e31bfaf7
Thibault Hermans <hermansthibault@gmail.com>
2020-11-13 16:37:52 +0100

    environment

--- technical-training-13.0-01-models/openacademy/models/session.py
+++ technical-training-13.0-01-models/openacademy/models/session.py
@@ -9,10 +9,16 @@ class Session(models.Model):
 
     _logger.info("YOOOOOOO")
     course_id = fields.Many2one(
-        string="Course", comodel_name="openacademy.course", required=True, default=lambda self:_logger.info(self.env))
+        string="Course", comodel_name="openacademy.course", required=True, default=_default_course_id)
 
     maester_id = fields.Many2one(
         string="Maester", comodel_name="res.partner", required=True)
 
     name = fields.Char(string="Session name", required=True)
     course_name = fields.Char(string="Course name", related="course_id.name")
+
+    
+    def _default_course_id(self):
+        _logger.info(self.env["openacademy.course"])
+        if self.env["openacademy.course"]:
+            return self.env["openacademy.course"]
