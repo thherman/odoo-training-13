04430e2983b3db2b39daff76208cc13b4b92569a
florvhee <florvhee@gmail.com>
2020-11-13 14:15:53 +0100

    inherit maester

--- technical-training-13.0-01-models/openacademy/models/maester.py
+++ technical-training-13.0-01-models/openacademy/models/maester.py
@@ -5,6 +5,7 @@ class Maester(models.Model):
     _name = "res.partner"
     _inherit = "res.partner"
     _description = "Maester assigned to a session"
+    
 
     session_ids = fields.One2many(
         comodel_name="openacademy.session", inverse_name="course_id", string="Sessions")
